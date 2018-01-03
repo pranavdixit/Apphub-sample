d43f75b8257bb0ec3e30251e5c6ebf616302eb10
pranav.dixit <pranav.dixit@equinox.com>
2018-01-03 13:33:58 +0530

    basic project

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>You are on emulator my friend.</Text>
+         <Image
+          style={{width: 400, height: 500}}
+          source={{uri: 'https://jscomplete.com/stf/images/workshops/react-native.png'}}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ HelloWorldApp.js
@@ -0,0 +1,10 @@
+mport React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
